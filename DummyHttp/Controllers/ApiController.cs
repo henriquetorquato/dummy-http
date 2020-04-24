0abae147369f9df0d42889bbc13de5caf1604afd
Henrique Torquato <henriquet@take.net>
2020-04-24 11:56:13 -0300

    Initial Commit

---
+++ DummyHttp/Controllers/ApiController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DummyHttp.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public ApiController(ILogger<ApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task PostAsync()
+        {
+            using var reader = new StreamReader(Request.Body);
+            var value = await reader.ReadToEndAsync();
+
+            _logger.LogInformation(value);
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "pong";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Dummy HTTP
+
+Dummy WebApi used to test WebHook's.
+
+## Ngrok
+
+```shell
+ngrok http -host-header="localhost:[port]" [port]
+```
